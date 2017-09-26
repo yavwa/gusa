bc20ebe6559b1920221b09b2903abc3642284d7f
yavwa <kmungasia@yahoo.com>
2017-09-26 21:33:26 +0300

    new
    social network

---
+++ configurations/ossn.config.db.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright 2014-2017 SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+
+// replace localhost with your database host name;
+$Ossn->host = 'localhost';
+
+// replace 3306 with your database host name;
+$Ossn->port = '3306';
+
+// replace gusaco_1 with your database username;
+$Ossn->user = 'gusaco_1';
+
+// replace zakuiba@2016 with your database password;
+$Ossn->password = 'zakuiba@2016';
+
+// replace gusaco_1 with your database name;
+$Ossn->database = 'gusaco_1';
